ec3ee2b4a07ec9d571f43ccb04d8f8c09af22690
parkerhutcheson <pdhutcheson@gmail.com>
2026-04-28 10:59:06 +0900

    Essential express server pieces

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.error(`Server initialized on port ${process.env.PORT}...`);
+});
+
+export default app;
