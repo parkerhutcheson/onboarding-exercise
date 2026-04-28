9bc566834ce1f7f2706bd79a52b87079f23e0409
parkerhutcheson <pdhutcheson@gmail.com>
2026-04-28 12:03:41 +0900

    Completed SQL queries, added some comments

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -6,3 +6,20 @@ LIMIT 10;
 -- 2.) Filter all customers with emails from @gmail.com
 SELECT * FROM customers
 WHERE email LIKE '%@gmail.com';
+
+-- 3.) Customers created each month in 2025
+SELECT MONTH(created_at) AS month, COUNT(*) AS total -- counts the number of customers in a month, assigned variable "month"
+FROM customers
+WHERE YEAR(created_at) = 2025
+GROUP BY month
+ORDER BY month;
+
+-- 4.) Find duplicate email addresses
+SELECT email, COUNT(*) AS total -- count occurrences
+FROM customers
+GROUP BY email
+HAVING COUNT(*) > 1; -- having filters after grouping
+
+-- 5.) All customers whose first name starts with "A"
+SELECT * FROM customers
+WHERE first_name LIKE 'A%';
